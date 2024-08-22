c62536f2daa1599914e923bad60da98dff61ca71
Junya Ogura <junya@sooey.com>
2024-08-22 13:05:24 +0900

    Add support for Rails 7.0

--- spec/dummy_app/config/application.rb
+++ spec/dummy_app/config/application.rb
@@ -4,4 +4,7 @@ require "rails"
 
 class DummyApp < Rails::Application
   config.eager_load = false
+
+  # Load defaults config for the bundled rails version.
+  config.load_defaults Rails::VERSION::STRING[0, 3]
 end
